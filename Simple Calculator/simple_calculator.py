94550ca9fbfd8aa86e63c7c81869532fbe394578
ni100234 <100220702+ni100234@users.noreply.github.com>
2022-02-24 09:53:17 +0545

    Update simple_calculator.py

--- Simple Calculator/simple_calculator.py
+++ Simple Calculator/simple_calculator.py
@@ -1,8 +1,7 @@
-
 from tkinter import *
  
 def iCalc(source, side):
-    storeObj = Frame(source, borderwidth=4, bd=4, bg="powder blue")
+    storeObj = Frame(source, borderwidth=4, bd=4, bg="white")
     storeObj.pack(side=side, expand =YES, fill =BOTH)
     return storeObj
  
@@ -18,10 +17,35 @@ class app(Frame):
         self.pack(expand = YES, fill =BOTH)
         self.master.title('Calculator')
  
-display = StringVar()
-    Entry(self, relief=RIDGE, textvariable=display,
-          justify='right'
-          , bd=30, bg="powder blue").pack(side=TOP,
-                                          expand=YES, fill=BOTH)
+        display = StringVar()
+        Entry(self, relief=RIDGE, textvariable=display,justify='right',bd=30, bg="white").pack(side=TOP,expand=YES, fill=BOTH)
+ 
+        for clearButton in (["C"]):
+            erase = iCalc(self, TOP)
+            for ichar in clearButton:
+                button(erase, LEFT, ichar, lambda storeObj=display, q=ichar: storeObj.set(''))
+ 
+        for numButton in ("789/", "456*", "123-", "0.+"):
+         FunctionNum = iCalc(self, TOP)
+         for iEquals in numButton:
+            button(FunctionNum, LEFT, iEquals,lambda storeObj=display, q=iEquals: storeObj.set(storeObj.get() + q))
+ 
+        EqualButton = iCalc(self, TOP)
+        for iEquals in "=":
+            if iEquals == '=':
+                btniEquals = button(EqualButton, LEFT, iEquals)
+                btniEquals.bind('<ButtonRelease-1>', lambda e,s=self,
+                                storeObj=display: s.calc(storeObj), '+')
+ 
+            else:
+                btniEquals = button(EqualButton, LEFT, iEquals,lambda storeObj=display, s=' %s ' % iEquals: storeObj.set
+                                    (storeObj.get() + s))
+ 
+    def calc(self, display):
+            try:
+                display.set(eval(display.get()))
+            except:
+                display.set("ERROR")
+ 
 if __name__=='__main__':
  app().mainloop()
